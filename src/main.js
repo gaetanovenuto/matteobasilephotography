c8020dbacb44d06fd36b05c8c7b3164c9e4b9508
gaetanovenuto <gaetanovenuto99@live.it>
2025-02-19 23:14:09 +0100

    Aggiunto bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import './assets/scss/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 const app = createApp(App)
 
